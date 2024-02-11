4628f824b2982d694a297c9d172709260b456f08
vitalyvitmens <otm_@tut.by>
2024-02-11 18:23:17 +0300

    chore(dist): деплоит проект

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import path from 'path';
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
-      'src': path.resolve(__dirname, './src'),
+      src: path.resolve(__dirname, './src'),
     },
   },
+  base:
+    process.env.NODE_ENV === 'production'
+      ? '/https://github.com/vitalyvitmens/rs-calculator/'
+      : '/',
   plugins: [react()],
 })
